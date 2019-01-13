884a2b44c4b41eff164f9424c3b6fd576e8f2afa
Sargam Chhabra <sargamchhabra15@gmail.com>
2019-01-13 18:09:30 +0530

    done

---
+++ charac.java
@@ -0,0 +1,10 @@
+class charac{
+	public static void main ( String[] args){
+	char ch = 'A';
+	int ascii = ch ;
+	int castAscii = (int)ch ;
+	System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+
+	}
+}
