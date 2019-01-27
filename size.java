85ad964a9833fe5a580575fe66731618ebe07395
Sargam Chhabra <sargamchhabra15@gmail.com>
2019-01-27 17:34:34 +0530

    done

---
+++ size.java
@@ -0,0 +1,6 @@
+class DataTypes{
+	public static void main( String[] args){
+	System.out.print("S.No   Data type   Size   MIN_VALUE   MAX_VALUE");
+	System.out.println("1  int  "+sizeof(int.class));
+	}
+}
