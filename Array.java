7cc71430c3f8d7fc0d7f8871336c67a3eee4275f
Sargam Chhabra <sargamchhabra15@gmail.com>
2019-02-04 22:51:53 +0530

    done

---
+++ Array.java
@@ -0,0 +1,9 @@
+import java.util.* ;
+class Array{
+	int i ;
+	int arr[] = new int[i];
+	Scanner input = new Scanner(System.in) ;
+	arr[] = input.nextInt();
+	System.out.println("enter value of i"+i);
+	System.out.println("enter 5 values in array ");
+}
